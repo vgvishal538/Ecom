fc5dfe9c3c9bb386935fb8ea338e23a3874e5d33
vgvishal538 <vgvishal538@gmail.com>
2022-09-05 17:03:01 +0530

    bottom nav added

--- lib/main.dart
+++ lib/main.dart
@@ -27,80 +27,59 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Splash'),
+      home:  MyHomePage(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyHomePage> createState() => HomeScreenWidget();
+}
+class HomeScreenWidget extends State<MyHomePage>{
+  int _selectedIndex = 0;
+   static const TextStyle optionStyle= TextStyle(fontSize: 20);
+   static const List<Widget> _widgetOptions = <Widget>[
+     Text('Home',style: optionStyle,),
+     Text("Bag",style: optionStyle,),
+     Text('Profile',style: optionStyle,)
+   ];
+
+   void _onItemTapped(int index){
+     setState(() {
+       _selectedIndex=index;
+     });
+   }
+
+   @override
+  Widget build(BuildContext context){
+     return Scaffold(
+       appBar: AppBar(
+         title: const Text('ECOM'),
+       ),
+       body: Center(
+         child: _widgetOptions.elementAt(_selectedIndex),
+       ),
+       bottomNavigationBar: BottomNavigationBar(
+         items: const <BottomNavigationBarItem>[
+           BottomNavigationBarItem(
+               icon: Icon(Icons.home),
+               label: 'Home'
+           ),
+           BottomNavigationBarItem(icon: Icon(Icons.shopping_cart),label: 'Cart'),
+           BottomNavigationBarItem(icon: Icon(Icons.supervised_user_circle_rounded),label: 'Profile'),
+         ],
+         currentIndex: _selectedIndex,
+         selectedItemColor: Colors.amber[800],
+         onTap:_onItemTapped,
+       ),
+     );
+   }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  void initstate() {
-    super.initState();
-    Timer(
-        Duration(seconds: 3),
-        () => Navigator.pushReplacement(
-            context, MaterialPageRoute(builder: (context)=>const SplashScreen(title: "Home"))));
-  }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-     
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          
-    
-        ),
-        
-      ),
-    );
-  }
-}
+
+
--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -2,8 +2,11 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 
 
+
+
 class SplashScreen extends StatelessWidget{
   const SplashScreen({Key? key, required String title}) : super(key: key);
+
 
    @override
   Widget build(BuildContext context) {
